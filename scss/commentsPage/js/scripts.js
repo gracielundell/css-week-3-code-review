3d518f099b657bd7cbc6b796cb4819926f2c565e
Gracie Lundell <gracielundell@gmail.com>
2016-02-28 17:55:36 -0800

    Add styling to new comments

--- scss/commentsPage/js/scripts.js
+++ scss/commentsPage/js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $("form").submit(function(event) {
-    $(".postedComments").prepend("<p>" +  $("#newComment").val() + "</p>");
+    $(".postedComments").prepend("<p>" +  $(".newComment").val() + "</p>");
 
     event.preventDefault();
   });
